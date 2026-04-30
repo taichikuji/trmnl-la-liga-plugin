e6dfbefc4bc833ced534f1e60bdc76675bd274ab
taichikuji <ivanperezfernandezsuper+github@gmail.com>
2026-04-30 08:53:39 +0000

    Simplify transform to single DoH status check

--- TRMNL/src/transform.js
+++ TRMNL/src/transform.js
@@ -1,18 +1,8 @@
 function transform(input) {
-  let isBlocked = false;
+  const isBlocked =
+    input?.Status === 0 &&
+    Array.isArray(input?.Answer) &&
+    input.Answer.length > 0;
 
-  if (input?.data) {
-    for (const item of input.data) {
-      const changes = item.stateChanges;
-      if (changes?.length > 0 && changes[changes.length - 1]?.state === true) {
-        isBlocked = true;
-        break; // Stop parsing immediately to save CPU time!
-      }
-    }
-  }
-
-  // Return the tiny, compressed payload for the TRMNL screen
-  return {
-    is_blocked: isBlocked
-  };
+  return { is_blocked: isBlocked };
 }
