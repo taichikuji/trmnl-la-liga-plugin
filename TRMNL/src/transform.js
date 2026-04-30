19aaecf81d7e22de430e42a57ae28daa30d59a15
taichikuji <ivanperezfernandezsuper+github@gmail.com>
2026-04-30 09:36:49 +0000

    Remove serverless transform function

--- TRMNL/src/transform.js
+++
@@ -1,8 +0,0 @@
-function transform(input) {
-  const isBlocked =
-    input?.Status === 0 &&
-    Array.isArray(input?.Answer) &&
-    input.Answer.length > 0;
-
-  return { is_blocked: isBlocked };
-}
